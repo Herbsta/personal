b13f75c8f280030c5e5a73018e940e033da231bb
Herbsta <55990383+Herbsta@users.noreply.github.com>
2024-08-17 19:32:16 +0300

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,10 @@ import ComingSoon from "@/components/ComingSoon";
 export default function NotFound() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Uh oh! You landed on a page that is not found...</h2>
-      <ComingSoon/>
+        <div>
+            <h2>Uh oh! You landed on a page that is not found...</h2>
+            <ComingSoon/>
+        </div>
     </main>
   )
 }
