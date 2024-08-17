9b6c691c8c412e6a208a85d8d0e9cabed551dea5
Herbsta <55990383+Herbsta@users.noreply.github.com>
2024-08-17 19:28:35 +0300

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import ComingSoon from "@/components/ComingSoon";
+
 export default function NotFound() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
