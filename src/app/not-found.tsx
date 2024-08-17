209601de0dca521b3fbf51e6370660ab9baf076e
Herbsta <55990383+Herbsta@users.noreply.github.com>
2024-08-17 19:26:12 +0300

    feat(404): added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2>Uh oh! You landed on a page that is not found...</h2>
+      <ComingSoon/>
+    </main>
+  )
+}
